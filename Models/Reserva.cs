363174559cdad7ef9b70a5bef980da2b693ac992
Tbrum <thayrony14@gmail.com>
2024-09-09 17:13:40 -0300

    entrega desafio 3

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
+            
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+
+                Exception ex = new Exception("Capacidade menor que o número de hóspedes");
+                Console.WriteLine(ex.Message);
             }
         }
 
@@ -35,26 +36,24 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+
             decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
 
-            return valor;
+            if (DiasReservados>=10)
+            {
+                valor = DiasReservados * Suite.ValorDiaria * 0.9m;
+                return valor;
+            }
+            else
+            {
+                return DiasReservados * Suite.ValorDiaria;
+            }
         }
     }
 }
